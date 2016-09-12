735ffbcf71d49ff42cf139b244fd26cf9823ca9b
spurdo <ananim359@gmail.com>
2016-09-12 11:57:01 +0700

    rename

--- app/src/main/java/com/babenko/movavitest/Data/Codes.java
+++
@@ -1,9 +0,0 @@
-package com.babenko.movavitest.Data;
-
-/**
- * Created by Roman Babenko (rbab@yandex.ru) on 8/31/2016.
- */
-public class Codes {
-    public final static int READ_EXTERNAL_STORAGE_CODE = 1;
-    public final static int PHOTO_GALLERY = 1;
-}
--- app/src/main/java/com/babenko/movavitest/Interfaces/SelectPIctureInterface.java
+++
@@ -1,8 +0,0 @@
-package com.babenko.movavitest.Interfaces;
-
-/**
- * Created by Roman Babenko (rbab@yandex.ru) on 8/31/2016.
- */
-public interface SelectPictureInterface {
-    void openGalery();
-}
